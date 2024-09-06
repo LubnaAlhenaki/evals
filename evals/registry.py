783e27f8cb6862d3b166f90793ba0e9e91fde219
Lubna Alhenaki <54324959+LubnaAlhenaki@users.noreply.github.com>
2024-09-06 20:14:08 +0300

    Update registry.py
    File "/usr/local/lib/python3.10/dist-packages/evals/registry.py", line 46
        ("gpt-4o": 128_000),
                 ^
    SyntaxError: invalid syntax

--- evals/registry.py
+++ evals/registry.py
@@ -43,7 +43,7 @@ def n_ctx_from_model_name(model_name: str) -> Optional[int]:
         ("gpt-4-32k-", 32768),
         ("gpt-4-", 8192),
 	("gpt-4o-mini", 128_000),
-	("gpt-4o": 128_000),
+	("gpt-4o", 128_000),
     ]
     MODEL_NAME_TO_N_CTX: dict[str, int] = {
         "ada": 2048,
